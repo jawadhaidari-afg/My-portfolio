9343e77c908c646ee9c40f76ecaca6d89557adc6
jawadhaidari-afg <jawadhaidari243@gmail.com>
2025-09-03 13:51:25 +0700

    Static Calculator

---
+++ ProfilePage/JavaScriptFiles/Calculator.js
@@ -0,0 +1,70 @@
+// Var, Let, const
+
+//Defining variables in JavaScript
+
+var global = 25;   // var variable 
+let local = 25;     //let variable
+const limited = 25;     //constant variable
+
+//JavaScript Naming
+
+var global = 25;
+var Global = 25;
+var GloBal = 25;
+var GLOBAL = 25;
+var globaL = 25;
+var global = 25;  // overide the prvious variables defined by var
+
+let Local = 30;
+let LOcal = 30;
+let LOCAL = 30;
+let locAL = 30;
+let LOCAl = 30;
+let LOCALL = 30; //errors and can't be oride unless be setted in different scope 
+
+let myfunction('') {
+    let LOCAL = 30;  // Despite variables have the resimble names but there is't any error due to different scopes
+}
+
+const limit = 40;
+const Limit = 40;
+const LIMIT = 40;
+const LIMIt = 40;
+
+//Dynamic Typing 
+        //JavaScript is a Dynamic (loosely Type language not Static Type Language)
+
+let myvariable = 30;    //numeric value
+let Myvariable = "String in JavaScript"; //string 
+let MyVariable = 33.2; // decimal number
+let MyVAriable = true; //boolean value
+var MyVAriablE = false; //boolean Value
+const MyVariAble = null; // null value
+
+
+//JavaScript Operators
+
+//Arithmatic Operators
+//Comparions Operators
+//Logical Operators
+//Assignment Operators
+//Conditional Operators
+//Ternary Operators
+
+//Arithmatic Operators (+,-,*,/,%.++,--)
+
+// + Addition
+
+let numberA = 40;
+let numberB = 60;
+let numberC = numberA+numberB;
+
+//document.getElementById("outputA").innerHTML = result;
+// console.log(result);
+
+//console.log(numberC);
+
+
+
+
+
