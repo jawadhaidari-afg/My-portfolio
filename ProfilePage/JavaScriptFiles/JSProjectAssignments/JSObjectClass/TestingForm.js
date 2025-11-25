62c9e94f8d5991bd7fb2d7f9025215f7e8b96cbf
jawadhaidari-afg <jawadhaidari243@gmail.com>
2025-11-25 18:55:09 +0700

    adding the Histoy JS Code

--- ProfilePage/JavaScriptFiles/JSProjectAssignments/JSObjectClass/TestingForm.js
+++ ProfilePage/JavaScriptFiles/JSProjectAssignments/JSObjectClass/TestingForm.js
@@ -32,32 +32,50 @@ document.getElementById("semester_a").textContent = ave;
 // Science DOM Section
 let Science1 = Number(localStorage.getItem("firstScience"));
 document.getElementById("sciencesemester1").innerHTML = Science1;
-
 let Science2 = Number(localStorage.getItem("secondScience"));
 document.getElementById("sciencesemester2").innerHTML = Science2;
-
 let Science3 = Number(localStorage.getItem("thirdScience"));
 document.getElementById("sciencesemester3").innerHTML = Science3;
-
 let Science4 = Number(localStorage.getItem("fourthScience"));
 document.getElementById("sciencesemester4").innerHTML = Science4;
-
 let scienceTotal = Science1 + Science2 + Science3 + Science4;
 document.getElementById("scienceTotal").innerHTML = scienceTotal;
-
 let ScienceAverage = scienceTotal/4;
 document.getElementById("scienceAverage").innerHTML = ScienceAverage;
 
 
 //Math DOM Section
-document.getElementById("mathSemester1");
-document.getElementById("mathSemester2");
-document.getElementById("mathSemester3");
-document.getElementById("mathSemester4");
+const Math_first = Number(localStorage.getItem("mathFirst"));
+document.getElementById("mathSemester1").innerHTML=Math_first;
+const Math_second= Number(localStorage.getItem("mathSecond"));
+document.getElementById("mathSemester2").innerHTML=Math_second;
+const Math_third = Number(localStorage.getItem("mathThird", score.value));
+document.getElementById("mathSemester3").innerHTML=Math_third;
+const Math_fourth = Number(localStorage.getItem("mathFourth", score.value));
+document.getElementById("mathSemester4").innerHTML= Math_fourth;
+let Math_total = Math_first+Math_second+Math_third+Math_fourth;
+document.getElementById("mathTotal").innerHTML=Math_total;
+let MathAverage = Math_total/4;
+document.getElementById("mathAverage").innerHTML=MathAverage;
+
+//History DOM Section
+let His_first=Number(localStorage.getItem("historyFirst",score.value));
+document.getElementById("historySemester1").innerHTML=His_first;
+let His_second=Number(localStorage.getItem("historySecond", score.value));
+document.getElementById("historySemester2").innerHTML=His_second;
+let His_third=Number(localStorage.getItem("historyThird",score.value));
+document.getElementById("historySemester3").innerHTML=His_third;
+let His_fourth=Number(localStorage.getItem("historyFourth",score.value));
+document.getElementById("historySemester4").innerHTML=score.value;
+let His_Total=His_first+His_second+His_third+His_fourth;
+document.getElementById("historyTotal").innerHTML=His_Total;
+let His_Average=His_Total/4;
+document.getElementById("historyAverage").innerHTML=His_Average;
 
 
 
 //Conditional Section
+//English Subject
 click.addEventListener("click", function () {
     if (semester.value == "First_term" && subject.value == "English") {
         localStorage.setItem("firstEnglish", score.value);
@@ -107,35 +125,54 @@ click.addEventListener("click", function () {
 
 //Math Conditional Section
 click.addEventListener("click",function(){
-    if(semester.value == "Math" && subject.value=="First_term"){
+    if(subject.value =="Math" && semester.value=="First_term"){
+        localStorage.setItem("mathFirst", score.value);
         document.getElementById("mathSemester1").innerHTML=score.value;
     }
-    else if (semester.value=="Math"&&subject.value=="Second_term"){
+    else if (subject.value=="Math"&&semester.value=="Second_term"){
+        localStorage.setItem("mathSecond",score.value);
         document.getElementById("mathSemester2").innerHTML=score.value;
     }
-    else if(semester.value=="Math"&&subject.value=="Third_term"){
+    else if(subject.value=="Math"&&semester.value=="Third_term"){
+        localStorage.setItem("mathThird", score.value);
         document.getElementById("mathSemester3").innerHTML=score.value;
     }
-    else(semester.value=="Math"&&subject.value=="Fourth_term")
+    else (subject.value=="Math"&&semester.value=="Fourth_term")
     {
-        document.getElementById("englishSemester4").innerHTML=score.value;
+        localStorage.setItem("mathFourth",score.value);
+        document.getElementById("mathSemester4").innerHTML=score.value;
     }
+    console.log(score.value)
 });
 
-// Average Section
-//  function averagescore(a,b,c,d){
-//         return (a+b+c+d)/4;
-//     }
-//     document.getElementById("semester_a").innerHTML=averagescore(firstMath,secondMath,thirdMath,fourthMath); 
-// console.log(averagescore(firstMath,secondMath,thirdMath,fourthMath));
+//History Conditional Section
+click.addEventListener("click",function(){
+    if(subject.value=="History"&&semester.value=="First_term"){
+        localStorage.setItem("histryFirst", score.value);
+        document.getElementById("historySemester1").innerHTML=score.value;
+    }
+    else if (subject.value=="History"&&semester.value=="Second_term"){
+        localStorage.setItem("historySecond",score.value);
+        document.getElementById("historySemester2").innerHTML=score.value;
+    }
+    else if (subject.value=="History"&&semester.value=="Third_term"){
+        localStorage.setItem("historyThird",score.value);
+        document.getElementById("historySemester3").innerHTML=score.value;
+    }
+    else if (subject.value=="History"&&semester.value=="Fourth_term"){
+        localStorage.setItem("historyFourth",score.value);
+        document.getElementById("historySemester4").innerHTML=score.value;
+    }
+    console.log(score.value);
+});
 
 
-// Egnlish Grouping Section
+// English Grouping Section
 let Average = firstEng + secondEng + thirdEng + fourthEng;
 if (ave == 100) {
     document.getElementById("semester_g").innerHTML = "A+";
 }
-else if (ave < 100 && ave > 89) {
+else if (ave < 100 && ave > 90) {
     document.getElementById("semester_g").innerHTML = "A";
     console.log(Average);
 }
@@ -154,7 +191,7 @@ else if (ave < 70 && ave > 0) {
 
 //Science Grouping Section
 let Sci_Ave = Science1 + Science2 + Science3 + Science4;
-if (ScienceAverage == 400) {
+if (ScienceAverage == 100) {
     document.getElementById("scienceGroup").innerHTML = "A+";
 }
 else if (scienceAverage < 100 && sciencesemester2 > 90) {
@@ -166,7 +203,47 @@ else if (sciencesemester2 < 90 && sciencesemester2 > 80) {
 else if (scienceAverage < 80 && scienceAverage > 70) {
     document.getElementById("scienceGroup").innerHTML = "C";
 }
-else (scienceAverage < 70 && scienceAverage > 0)
+else if (scienceAverage < 70 && scienceAverage > 0)
 {
     document.getElementById("scienceGroup").innerHTML = "Failled";
 }
+console.log(Sci_Ave);
+
+//Math Grouping Section
+
+    let Math_Average = Math_first+Math_second+Math_third+Math_fourth;
+    if(MathAverage==100){
+        document.getElementById("mathGroup").innerHTML="A+";
+    }
+    else if (MathAverage<100&&MathAverage>90){
+        document.getElementById("mathGroup").innerHTML="A";
+    }
+    else if (MathAverage<90&&MathAverage>80){
+        document.getElementById("mathGroup").innerHTML="B";
+    }
+    else if (MathAverage<80&&MathAverage>70){
+        document.getElementById("mathGroup").innerHTML="C";
+    }
+    else if (MathAverage<70&&MathAverage>0)
+    {
+        document.getElementById("mathGroup").innerHTML="Failled";
+    }
+    console.log(MathAverage);
+
+    //History Grouping Section
+    let History_Average=His_first+His_second+His_third+His_fourth;
+    if(His_Average==100){
+        document.getElementById("historyGroup").innerHTML="A+";
+    }
+    else if(His_Average<100&&His_Average>90){
+        document.getElementById("historyGroup").innerHTML="A";
+    }
+    else if (His_Average<90&&His_Average>80){
+        document.getElementById("historyGroup").innerHTML="B";
+    }
+    else if(His_Average<80&&His_Average>70){
+        document.getElementById("historyGroup").innerHTML="C";
+    }
+    else if(His_Average<70&&His_Average>0){
+        document.getElementById("historyGroup").innerHTML="Failled";
+    }
