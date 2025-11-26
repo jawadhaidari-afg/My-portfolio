c0aa63c4f1e57751ad382efef95311646b325d1f
jawadhaidari-afg <jawadhaidari243@gmail.com>
2025-11-26 18:36:25 +0700

    adding JS Codes of Weather Converter

--- ProfilePage/JavaScriptFiles/JSProjectAssignments/JSObjectClass/WeatherConverter.js
+++ ProfilePage/JavaScriptFiles/JSProjectAssignments/JSObjectClass/WeatherConverter.js
@@ -1,3 +1,20 @@
 //Input DOM Sections
-document.getElementById("Input_temperature");
-document.getElementById("temSymbol");
+let Temperature = Degree;
+document.getElementById("Input_temperature").innerHTML=Degree;
+let Tem_symbol=Symbol;
+document.getElementById("temSymbol").innerHTML=Symbol;
+let Calculate=Counting;
+document.getElementById("click").innerHTML=Counting;
+
+// Temperature Conditional Section
+let Celsius=0;
+let Farenhit=32;
+click.addEventListener("click",function(){
+    if(Input_temperature.value=="C"){
+        return Degree-32;
+        console.log()
+    }
+    else if (addEventListener==F){
+        return Farenhit+32;
+    }
+})
